bc99b7f10dbeeee40adec1180abea1673a974247
Javaun Moradi <jmoradi@javauns-mbp.mynetworksettings.com>
2024-03-25 12:12:15 -0400

    renaming package, renumbering

--- src/formulaic/__init__.py
+++
@@ -1,4 +0,0 @@
-from .formulaic import Formula
-from .formulaic import load_formula
-from .formulaic import save_output
-from .openclient import OpenClient
---
+++ src/formulaic_ai/__init__.py
@@ -0,0 +1,4 @@
+from .formulaic_ai import Formula
+from .formulaic_ai import load_formula
+from .formulaic_ai import save_output
+from .openclient import OpenClient
