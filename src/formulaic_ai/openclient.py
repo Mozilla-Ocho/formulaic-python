f435d03a0f606967e6d1bc5ef618df62ddd67e5e
Gabriel <ghabayeb@mozilla.com>
2024-04-11 11:59:35 -0400

    Fix chat function

--- src/formulaic_ai/openclient.py
+++ src/formulaic_ai/openclient.py
@@ -15,17 +15,15 @@ class OpenClient:
         self.model = model
         self.client = client
 
-    def send_message(self, message, printable=True, direct=False):
+    def send_message(self, message, printable=True):
         """append to messages, print, send it."""
         self.messages.append({"role": "user", "content": message})
-        print(f"Messages: {self.messages}\n")
-        print("Model: ", self.model)
-        print(type(self.messages))
+
         completion = self.client.chat.completions.create(
             model=self.model,
             messages=self.messages,
         )
-        print(f"completion: {completion}\n")
+
         # get clean answer, append it to messages, print
         answer = completion.choices[0].message.content
         self.messages.append({"role": "assistant", "content": answer})
@@ -34,23 +32,32 @@ class OpenClient:
         if printable:
             print(f"Assistant: {answer}\n")
 
+        return completion
+
     def run(
         self,
         printable=True,
     ):
         """Run the prompts and responses."""
+        self.responses = []
         prompts = self.formula.script.get("script").get("sequences")
+        completion = None
         for p in prompts:
-            self.send_message(p, printable)
+            completion = self.send_message(p, printable)
+
+        if printable:
+            print(completion.model_dump())
+
+        return self.responses
 
     def chat(self, printable=False):
-        """Start a chat session."""
+        """Start an interactable chat session on command line"""
         self.run(printable)
         next_message = "a"
         while next_message:
             next_message = input("> ")
             if next_message:
-                self.send_message(next_message, printable, direct=True)
+                self.send_message(next_message, printable)
 
     def __enter__(self):
         return self
